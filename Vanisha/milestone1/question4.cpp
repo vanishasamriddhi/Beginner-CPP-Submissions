e7aafc02d060238c6c55ef76162fd5fd7e396702
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-01-26 23:16:56 +0530

    Create question4.cpp

---
+++ Vanisha/milestone1/question4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ using namespace std;
+ 
+ int main()
+ {
+ 	int a;
+ 	cin>>a;
+ 	if (a<0)
+ 	cout<<"The given number is negative"<<endl;
+ 	else if (a==0)
+ 	cout<<"The given number is zero"<<endl;
+ 	else
+ 		cout<<"The given number is positive"<<endl;
+ 	return 0;
+ }
