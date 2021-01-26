a8b278e472293d7e442f41f60fbde7ecd9741ad6
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-01-26 23:09:18 +0530

    Create question2.cpp

---
+++ Vanisha/milestone1/question2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ using namespace std;
+ 
+ int main()
+ {
+ 	int a,b;
+ 	cin>>a>>b;
+ 	if (a>b)
+ 	cout<<"The maximum of the two numbers is"<<a<<endl;
+ 	else
+ 		cout<<"The maximum of the two numbers is"<<b<<endl;
+ 	return 0;
+ }
