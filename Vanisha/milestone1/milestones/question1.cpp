d01ca6730140e0c3756f3f2da5b2f0c1bbb04ad0
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-01-27 12:15:25 +0530

    Update question1.cpp

--- Vanisha/milestone1/milestones/question1.cpp
+++ Vanisha/milestone1/milestones/question1.cpp
@@ -5,11 +5,13 @@ include <iostream>
  int main()
  {
  	int i=1,n;
- 	cout<<"Enter the last digit of the series:"<<endl;
-	 	cin>>n;
+ 	           cout<<"Enter the last digit of the series:"<<endl;
+	 	   cin>>n;
+	 
+	 
 	 	
-	 		cout<<"The given series is: "<<endl;
-	 	 for(i ;i<=n; i++)
+	              cout<<"The given series is: "<<endl;
+	 for(i ;i<=n; i++)
  	{
  	
 	 cout<<i<<endl;
