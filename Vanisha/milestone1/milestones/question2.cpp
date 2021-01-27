7a06a1e2f5edac77f3c7200d5ec1938cfcf467ac
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-01-27 12:12:46 +0530

    Create question2.cpp

---
+++ Vanisha/milestone1/milestones/question2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>include <iostream>
+ using namespace std;
+ 
+ int main()
+ {
+ 	int i=1,n;
+ 	cout<<"Enter the last digit of the series:"<<endl;
+	 	cin>>n;
+	 	
+	 		cout<<"The given series is: "<<endl;
+	 	 for(i ;i<=n; i++)
+ 	{
+ 	
+	 cout<<i<<endl;
+ 	
+ 	}
+ 	return 0;
+ }
