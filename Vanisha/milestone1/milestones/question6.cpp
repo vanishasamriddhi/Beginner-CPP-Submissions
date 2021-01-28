12300d17df556d2cb1134f8ab924adda896739dc
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-01-28 19:30:31 +0530

    Create question6.cpp

---
+++ Vanisha/milestone1/milestones/question6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ using namespace std;
+ int main()
+ {
+     int i,n;
+     cout<<"ENTER THE LAST DIGIT:"<<endl;
+     cin>>n;
+
+    { 
+   
+    	cout<<"SUM OF ALL THE NATURAL NUMBERS ARE : "<<n*n/2+n/2<<endl;}
+    
+    
+    return 0;
+ }
+ 
