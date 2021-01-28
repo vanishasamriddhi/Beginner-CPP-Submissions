f67a8e463134a6ad47ade605dafde33fc82d264d
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-01-28 19:21:01 +0530

    Create question4.cpp

---
+++ Vanisha/milestone1/milestones/question4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ using namespace std;
+ int main()
+ {
+     int i;
+    for(i=2 ;i<=100; i++)
+    { 
+    if(i%2==0)
+    {	cout<<(i)<<endl;}
+    else 
+    cout<<" "<<endl;
+	}
+    
+    return 0;
+ }
+ 
