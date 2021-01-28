cd51b707110c73035189435c284a7f034b193915
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-01-28 22:46:04 +0530

    Create question9.cpp

---
+++ Vanisha/milestone1/milestones/question9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ using namespace std;
+ int main()
+ {
+     int i,n, sum;
+     cout<<"ENTER THE DIGIT:"<<endl;
+     cin>>n;
+     for(i=2; i<=n; i+=2) 
+    { 
+
+ cout<<i*n
+  
+  }
+  
+    return 0;
+ }
