3f9baa8f1f3d2f74f7a03bb02c0f31d7919227a7
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-05-31 18:38:48 +0530

    Create 1.cpp

---
+++ Vanisha/milestone 10/1.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+void sum(){
+   double num1, num2, answer;
+    cin>>num1>>num2;
+   answer = num1+ num2;
+   cout<<answer;
+   
+}
+
+int main()
+{
+   
+    sum();
+
+    return 0;
+}
