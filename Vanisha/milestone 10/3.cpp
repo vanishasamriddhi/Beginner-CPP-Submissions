61a0b3b2c290da415af8ba64bb302d229420eeaa
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-05-31 18:43:29 +0530

    Create 3.cpp

---
+++ Vanisha/milestone 10/3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+void sum(){
+   double num1, area, circumference;
+    cin>>num1;
+    area = 3.14*num1*num1;
+   circumference = 2*3.14*num1;
+   cout<<area<<"  "<<circumference;
+   
+}
+
+int main()
+{
+   
+    sum();
+
+    return 0;
+}
