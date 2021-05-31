573edf42fcfb3175e6d94f70bc1d735b37563642
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-05-31 18:57:16 +0530

    Create 5.cpp

---
+++ Vanisha/milestone 10/5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+void isOddorEven(){
+   int num1;
+    cin>>num1;
+ if (num1%2==0)
+ cout<<num1 <<"is even"<<endl;
+  
+  else
+  cout<<num1 <<"is odd"<<endl;
+}
+
+
+
+int main()
+{
+   
+    isOddorEven();
+    
+
+    return 0;
+}
