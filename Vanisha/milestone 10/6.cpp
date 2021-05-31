b620198815800ecceea552f0b6deadef245a4093
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2021-05-31 19:04:47 +0530

    Create 6.cpp

---
+++ Vanisha/milestone 10/6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+void isvoter(){
+   int num1;
+   cout<<"Enter your age ";
+    cin>>num1;
+ if (num1>=18 )
+ cout<<"You are eligibe for vote"<<endl;
+  
+  else
+  cout<<"You are not eligibe for vote"<<endl;
+}
+
+
+
+int main()
+{
+   
+    isvoter();
+    
+
+    return 0;
+}
